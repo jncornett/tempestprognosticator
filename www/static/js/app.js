8a3b6b0d4abb71b75c1e9c227ed477e35ad00f47
Joel Cornett <joel.cornett@gmail.com>
2017-02-20 00:10:57 -0500

    add popover help and more glue

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -25,17 +25,25 @@ const QuestionTypeModel = Backbone.Model.extend({
   idAttribute: 'name'
 });
 
+const ExportTypeModel = Backbone.Model.extend({
+  defaults: {
+    name: '',
+  },
+  idAttribute: 'name'
+});
+
 const AppSettings = Backbone.Model.extend({
   defaults: function() {
     return {
       words: new Set(),
       totalQuestions: 0,
       usedWords: 0,
-      generated: [],
+      questions: [],
     };
   },
   initialize: function() {
     this.questionTypes = new Backbone.Collection({model: QuestionTypeModel});
+    this.exportTypes = new Backbone.Collection({model: ExportTypeModel});
   }
 });
 
@@ -65,27 +73,71 @@ $(() => {
   });
 
   const QuestionTypeView = Backbone.View.extend({
-    // TODO add check/uncheck event
+    tagName: 'div',
     template: Handlebars.compile($('#question-type-setting').html()),
-    render: function() {
-      this.$el = $('<div>').append(this.template(this.model.toJSON()));
-      this.el = this.$el[0];
-      return this;
+    events: {
+      'click .ctrl-enable': 'onClick'
+    },
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.render);
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$('.ctrl-help').webuiPopover({
+        title: this.model.get('name'),
+        content: this.model.get('help')
+      });
+    },
+    onClick: function(e) {
+      this.model.set('enabled', $(e.currentTarget).prop('checked'));
     }
   });
 
   const SettingsView = Backbone.View.extend({
+    events: {
+      'input .ctrl-num-questions': 'onNumQuestionsInput',
+      'input .ctrl-num-words': 'onNumWordsInput'
+    },
     initialize: function(options) {
+      this.$('.ctrl-num-questions').val(this.model.get('totalQuestions'));
+      this.$('.ctrl-num-words').val(this.model.get('usedWords'));
       this.listenTo(this.model.questionTypes, 'add', this.addQuestionType);
+      this.listenTo(this.model, 'change', this.render);
+      this.render();
+    },
+    render: function() {
+      this.$('.ind-total-words').text(this.model.get('words').length);
+      this.$('.ctrl-num-words').prop('max', this.model.get('words').length);
+      this.$('.ind-num-questions').text(this.model.get('totalQuestions'));
+      this.$('.ind-num-words').text(this.$('.ctrl-num-words').val());
     },
     addQuestionType: function(model) {
       const view = new QuestionTypeView({model: model});
-      const el = this.$('#question-types-container');
-      el.append(view.render().el);
+      this.$('#question-types-container').append(view.el);
+    },
+    onNumQuestionsInput: function(e) {
+      const val = parseInt($(e.currentTarget).val());
+      this.model.set('totalQuestions', val);
+    },
+    onNumWordsInput: function(e) {
+      const val = parseInt($(e.currentTarget).val());
+      this.model.set('usedWords', val);
     }
   });
 
-  const ExportView = Backbone.View.extend({});
+  const ExportView = Backbone.View.extend({
+    events: {
+      'click .ctrl-download': 'onDownload'
+    },
+    initialize: function(options) {
+      this.listenTo(this.model.exportTypes, 'add', this.addExportType);
+      console.log(options.api);
+    },
+    addExportType: function(model) {
+      this.$('.ctrl-export-types').append($('<option>').text(model.get('name')));
+    },
+    onDownload: function() {
+      console.log('onDownload');
+    }
+  });
 
   const CreateView = Backbone.View.extend({
     initialize: function(options) {
@@ -98,24 +150,32 @@ $(() => {
       }));
       this.exportView = new ExportView({
         el: this.$('#export'),
-        model: this.model
+        model: this.model,
+        api: options.api
       });
     }
   });
 
-  const PreviewView = Backbone.View.extend({});
+  const PreviewView = Backbone.View.extend({
+    initialize: function(options) {
+      this.listenTo(this.model, 'change:questions', this.render);
+      console.log(options.api);
+    },
+    render: function() {
+      console.log('preview render');
+    }
+  });
 
   const AppView = Backbone.View.extend({
     initialize: function(options) {
       this.createView = new CreateView(_.extend(options, {
         el: this.$('#create'),
+        api: options.api
       }));
       this.previewView = new PreviewView({
         el: this.$('#preview'),
-        model: this.model
-      });
-      this.listenTo(this.model, 'change', function() {
-        console.log('change', arguments);
+        model: this.model,
+        api: options.api
       });
     }
   });
@@ -123,6 +183,13 @@ $(() => {
   const app = new AppView({
     el: $('#app'),
     model: new AppSettings,
+    api: new DictionaryApi
   });
-  app.model.questionTypes.add({name: 'Foo'});
+  app.model.questionTypes.add({
+    name: 'Foo',
+    help: 'Some help'
+  });
+  app.model.exportTypes.add({
+    name: 'Javascript (JSON)'
+  });
 });
